48d96a4539170f0ebce14e93ef68492dcd800e99
kook2468 <cdg9808@naver.com>
2024-11-08 14:54:25 +0900

    test: 리액트 서버/클라이언트 컴포넌트 학습 - 최대한 많은 컴포넌트를 서버 컴포넌트로 유지시켜야함. (JS Bundle의 용량을 줄여야함) - 서버 컴포넌트에서는 브라우저에서 실행될 컴포넌트가 포함되면 안됨. - 클라이언트 컴포넌트는 2번 실행됨 (서버-사전렌더링, 브라우저-하이드레이션) - 클라이언트 컴포넌트에서 서버 컴포넌트 import 시 Next.js에서 자동으로 클라이언트 컴포넌트로 변경함. - 서버 컴포넌트에서 클라이언트 컴포넌트에게 직렬화 되지 않는 Props는 전달 불가능함.

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,7 +1,9 @@
+import SearchBar from "./searchbar";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <div>임시 서치바</div>
+      <SearchBar />
       {children}
     </div>
   );
