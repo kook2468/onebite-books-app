ce0497a0dd99848274e5cf6efdd392a75c5d5d47
kook2468 <cdg9808@naver.com>
2024-11-10 23:48:21 +0900

    test: 네비게이팅 학습
    - App Router 역시 CSR로 네비게이팅 처리함.
    - 페이지의 데이터는 서버 컴포넌트는 RSC payload로, 클라이언트 컴포넌트는 JS Bundle로 나뉘어서 동시에 전달됨.

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,18 +1,24 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function SearchBar() {
   const [search, setSearch] = useState("");
+  const router = useRouter(); // next/navigation 사용 : app router 버전의 패키지
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
+  const onSubmit = () => {
+    router.push(`/search?q=${search}`);
+  };
+
   return (
     <div>
       <input value={search} onChange={onChangeSearch} />
-      <button>검색</button>
+      <button onClick={onSubmit}>검색</button>
     </div>
   );
 }
