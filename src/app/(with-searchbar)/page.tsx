2508191798ac37e06ce93242ca965093c010f8a4
kook2468 <cdg9808@naver.com>
2024-11-13 18:52:36 +0900

    feat: 페이지 스트리밍 적용 - loading.tsx 파일은 페이지 컴포넌트가 로딩중일때 표시됨. - asyndc 키워드가 붙은 페이지 컴포넌트에만 스트리밍을 제공함. - 해당 경로 아래 모든 페이지 컴포넌트들이 스트리밍 되도록 설정됨. - 브라우저의 queryString이 변경될때는 트리거되지 않음.

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,20 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+
+//export const dynamic = "error";
+// 라우트 세그먼트 옵션
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+// 1. auto : 기본값, 아무것도 강제하지 않음
+// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static : 페이지를 강제로 Static 페이지로 설정
+//   => 이떄 queryString 같은 동적 값은 전부 undefinded를 반환하도록 설정됨
+//   => cache 설정도 no-store 라면 강제로 캐싱되도록 변경됨 (force-cache)
+// 4. error : 페이지를 강제로 Static 페이지로 설정 + 설정하면 안되는 이유가 있다면 빌드 오류를 발생시킴
 
 async function AllBooks() {
+  await delay(1500); //1.5초 딜레이
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" }
---
+++ src/app/(with-searchbar)/search/loading.tsx
@@ -0,0 +1,4 @@
+// 대체 UI
+export default function Loading() {
+  return <div>Loading ...</div>;
+}
