af48b5c78b95c8fac6a441d2aaaea6a1295df523
kook2468 <cdg9808@naver.com>
2024-11-14 11:05:36 +0900

    feat: 컴포넌트 스트리밍 적용 - Suspense 로 컴포넌트 단위로 스트리밍 적용 - search page : suspense의 key 값이 달라질때마다 다시 fallback 상태로 돌아감 - index page : 강제로 dynamic 페이지로 설정하고(라우트 세그먼트 옵션), 컴포넌트별 병렬로 각각 완료되면 렌더링됨

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -2,6 +2,7 @@ import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
 //export const dynamic = "error";
 // 라우트 세그먼트 옵션
@@ -32,6 +33,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -48,16 +50,22 @@ async function RecoBooks() {
   );
 }
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- src/app/(with-searchbar)/search/loading.tsx
+++
@@ -1,4 +0,0 @@
-// 대체 UI
-export default function Loading() {
-  return <div>Loading ...</div>;
-}
